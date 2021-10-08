689f56091bd54b2202616380749ded298a46a2f6
masoncfrancis <masoncfrancis@gmail.com>
2021-10-08 14:25:34 -0600

    Added more code and html to demonstrate objects and math.

--- InClassCode/Chapter4/chap4.js
+++ InClassCode/Chapter4/chap4.js
@@ -1,12 +1,33 @@
-
 function helloWorld() {
     document.getElementById('demo').innerHTML = 5 + 6;
 }
 
 // Being used to demonstrate datatype properties of javascript
-function showH1(width, height) {
+function showH1() {
     var number = 5;
     var word = "Hello";
-    document.getElementById('theArea').innerHTML = typeof(word);
+    document.getElementById('theArea').innerHTML = typeof (word);
 
 }
+
+// Prompts the user for variables found in the quadratic equation
+// and solves it, then presents it to the user
+function quadraticEquation() {
+    // note: Still need to parse user input into integers
+    var a = prompt("Enter an a");
+    var b = prompt("Enter an b");
+    var c = prompt("Enter an c");
+    var first = (-b + Math.sqrt(b*b-4*a*c))/(2*a);
+    var second = (-b - Math.sqrt(b*b-4*a*c))/(2*a);
+    document.getElementById('quadEqResult').innerHTML = `The results are ${first} and ${second}`;
+
+}
+
+// Demonstrates creating an Object with a function
+function Book(title, chapters, color){
+    this.title = title;
+    this.chapters = chapters;
+    this.color = colors;
+}
+
+
