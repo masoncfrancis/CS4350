d833358725a4c3e088449974557e06f164d114f9
masoncfrancis <masoncfrancis@gmail.com>
2021-10-18 14:50:03 -0600

    Added some input validation for fields on nochange.html

--- InClassCode/Chapter5/nochange.js
+++ InClassCode/Chapter5/nochange.js
@@ -5,6 +5,19 @@ function computeCost() {
     var hat = document.getElementById("hat").value; // $13.95
     var gloves = document.getElementById("gloves").value; // $9.59
 
+    if (isNaN(robes)){
+        document.getElementById("cost").value = "Fix robes value";
+        return;
+    }
+    if (isNaN(hat)){
+        document.getElementById("cost").value = "Fix hat value";
+        return;
+    }
+    if (isNaN(gloves)){
+        document.getElementById("cost").value = "Fix gloves value";
+        return;
+    }
+
     var totalPrice = 32.49 * robes + 13.95 * hat + 9.59 * gloves;
 
     document.getElementById("cost").value = totalPrice.toFixed(2);
